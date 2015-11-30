122784cbd3ff7db79db778d205e22a840d76239f
Cade Nichols <cade@cadenichols.com>
2015-11-30 12:06:54 -0800

    init

---
+++ client/js/module.js
@@ -0,0 +1,16 @@
+'use strict';
+
+let app = angular.module('socialLogin', ['satellizer', 'ui.router']);
+
+app.config(function($stateProvider, $urlRouterProvider, $authProvider) {
+  $urlRouterProvider.otherwise('/');
+  $stateProvider
+    .state('home', { url: '/', templateUrl: 'partials/home.html'})
+    .state('login', { url: '/login', templateUrl: 'partials/login.html', controller: 'loginCtrl'})
+
+
+    $authProvider.github({
+      clientId: 'a24f8ae4f7f98525f469'
+    });
+});
+
